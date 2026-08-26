3ce9b898903186631ddd8636b4a8f0cf3b46517d
Chinmayananda Dhupad <chinmaydhupad@gmail.com>
2026-08-26 11:38:58 +0530

    written struct node and created head of the singly linked list:

--- list.h
+++ list.h
@@ -1,4 +1,9 @@
 #ifndef LIST_H
 #include LIST_H
 
+typedef struct Node{
+    int data;
+    struct Node* next;
+};
+
 #endif
--- main.c
+++ main.c
@@ -1,1 +1,8 @@
+#include <stdio.h>
+#include "list.h"
 
+int main() {
+    Node* head = NULL;
+    
+    return 0;
+}
