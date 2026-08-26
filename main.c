8a90444f2e139ea7aff956f029721f19d098d0aa
Chinmayananda Dhupad <chinmaydhupad@gmail.com>
2026-08-26 12:07:21 +0530

    written main.c

--- main.c
+++ main.c
@@ -3,6 +3,15 @@
 
 int main() {
     Node* head = NULL;
-    
+
+    head = insert(head, 10);
+    head = insert(head, 20);
+    head = insert(head, 30);
+
+    printf("Linked list: ");
+    display(head);
+
+    free(head);
+    head = NULL;
     return 0;
 }
